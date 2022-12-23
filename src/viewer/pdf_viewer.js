3bd969221f517be16c17eb38460cc907d168bc5e
scmyers11 <scmyers11@gmail.com>
2022-12-23 14:05:56 -0600

    Updates pdf viewer for better exports

--- src/viewer/pdf_viewer.js
+++ src/viewer/pdf_viewer.js
@@ -8,12 +8,10 @@ import PDFPageView from './module.js';
 import EventBus from './module.js';
 
 delete window['pdfjs-dist/build/pdf'];
-export {
-  DefaultAnnotationLayerFactory,
-  DefaultTextLayerFactory,
-  PDFFindController,
-  PDFLinkService,
-  PDFPageView,
-  EventBus
-};
+export const DefaultAnnotationLayerFactory = DefaultAnnotationLayerFactory;
+export const DefaultTextLayerFactory = DefaultTextLayerFactory;
+export const PDFFindController = PDFFindController;
+export const PDFLinkService = PDFLinkService;
+export const PDFPageView = PDFPageView;
+export const EventBus = EventBus;
 export default viewer;
