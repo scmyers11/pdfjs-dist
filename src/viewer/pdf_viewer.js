4084ffdedab475a7cf39a3cce8eb91fd3212a496
scmyers11 <scmyers11@gmail.com>
2022-12-23 09:32:23 -0600

    Export more things from viewer

--- src/viewer/pdf_viewer.js
+++ src/viewer/pdf_viewer.js
@@ -1,5 +1,19 @@
-import "./util.js";
-import viewer from "./module.js";
+import './util.js';
+import viewer from './module.js';
+import DefaultAnnotationLayerFactory from './module.js';
+import DefaultTextLayerFactory from './module.js';
+import PDFFindController from './module.js';
+import PDFLinkService from './module.js';
+import PDFPageView from './module.js';
+import EventBus from './module.js';
 
-delete window["pdfjs-dist/build/pdf"];
+delete window['pdfjs-dist/build/pdf'];
+export {
+  DefaultAnnotationLayerFactory,
+  DefaultTextLayerFactory,
+  PDFFindController,
+  PDFLinkService,
+  PDFPageView,
+  EventBus
+};
 export default viewer;
