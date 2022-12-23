c0a30465d4ed105b53d4ad4b96bbbe786994db62
scmyers11 <scmyers11@gmail.com>
2022-12-23 14:28:28 -0600

    Fix an oops

--- src/viewer/pdf_viewer.js
+++ src/viewer/pdf_viewer.js
@@ -1,11 +1,5 @@
 import './util.js';
 import viewer from './module.js';
-import DefaultAnnotationLayerFactory from './module.js';
-import DefaultTextLayerFactory from './module.js';
-import PDFFindController from './module.js';
-import PDFLinkService from './module.js';
-import PDFPageView from './module.js';
-import EventBus from './module.js';
 
 delete window['pdfjs-dist/build/pdf'];
 export const DefaultAnnotationLayerFactory =
